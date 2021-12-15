07bedaef22c7845e1b5cf62ef17ca8fef8b5fdb3
Stassras <stasbarinoff@gmail.com>
2021-12-16 02:22:39 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   envDir: './',
-  base: './restoran',
+  base: './',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
